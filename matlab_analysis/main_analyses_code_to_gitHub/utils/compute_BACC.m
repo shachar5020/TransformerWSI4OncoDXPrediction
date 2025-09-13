b553ac03d6ca5b74686cca68e3c8a6308068bc43
shachar5020 <62300490+shachar5020@users.noreply.github.com>
2025-09-13 11:29:01 +0300

    Delete matlab_analysis/main_analyses_code_to_gitHub directory

--- matlab_analysis/main_analyses_code_to_gitHub/utils/compute_BACC.m
+++
@@ -1,5 +0,0 @@
-function BACC = compute_BACC(label_pred, label_true)
-
-a = sum(label_pred == 1 & label_true == 1)/sum(label_true == 1);
-b = sum(label_pred == 0 & label_true == 0)/sum(label_true == 0);
-BACC = (a+b)/2;
